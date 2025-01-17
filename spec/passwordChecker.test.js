285d5f5a9742707479c5890860911b3fa240be3d
gauthier <alicia.gauthier@student.vinci.be>
2025-01-17 17:24:32 +0100

    add: contains a digit test

--- spec/passwordChecker.test.js
+++ spec/passwordChecker.test.js
@@ -11,7 +11,7 @@ describe("verify that a string is a valid password  ", () => {
 
   it('should return false if password does not contain a special character', function () {
     const main = new Main();
-    
+
     const result = main.validatePassword('abcdef123');
     expect(result).toBe(false);
   });
@@ -21,6 +21,13 @@ describe("verify that a string is a valid password  ", () => {
     const result = main.validatePassword('Password@@');
     expect(result).toBe(false);
 });
+
+it('should return true if password does not contain "IPL"', function () {
+  const main = new Main();
+  const result = main.validatePassword('Password1@@');
+  expect(result).toBe(true);
+});
+
 
 
 
