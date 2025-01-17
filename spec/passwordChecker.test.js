82bd53c0e54a88519adac5e48201050462cfd83b
gauthier <alicia.gauthier@student.vinci.be>
2025-01-17 17:23:32 +0100

    add: no number test

--- spec/passwordChecker.test.js
+++ spec/passwordChecker.test.js
@@ -11,10 +11,17 @@ describe("verify that a string is a valid password  ", () => {
 
   it('should return false if password does not contain a special character', function () {
     const main = new Main();
-
+    
     const result = main.validatePassword('abcdef123');
     expect(result).toBe(false);
   });
+
+  it("should return false if password does not contain a number", function () {
+    const main = new Main();
+    const result = main.validatePassword('Password@@');
+    expect(result).toBe(false);
+});
+
 
 
 
--- src/passwordChecker.js
+++ src/passwordChecker.js
@@ -3,6 +3,8 @@ export class Main{
     if(password.length < 8) return false;
     const specialCharacter = /[!@#$%^&*(),.?":{}|<>]/;
     if(!specialCharacter.test(password)) return false;
+    const number = /[0-9]/; 
+    if (!number.test(password)) return false;
     return true;
   }
 }
